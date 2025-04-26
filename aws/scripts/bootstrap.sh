9c5cc411c9e8c5aa74e195f714dd1ac1155df415
guessi <guessi@gmail.com>
2025-04-27 02:11:10 +0800

    Simplify docker setup

--- aws/scripts/bootstrap.sh
+++ aws/scripts/bootstrap.sh
@@ -12,51 +12,27 @@ sudo apt update
 sudo DEBIAN_FRONTEND=noninteractive apt install -y \
     apt-transport-https ca-certificates curl gnupg
 
+# docker's official GPG key
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
 # setup docker repository
-curl -fsSL "https://download.docker.com/linux/ubuntu/gpg" | sudo apt-key add -qq -
-echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu ${UBUNTU_CODE_NAME} stable" | sudo tee /etc/apt/sources.list.d/docker.list
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu ${UBUNTU_CODE_NAME} stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# setup docker daemon
-# HINT: pinned docker-ce version to avoid breaking changes
-
-case ${UBUNTU_CODE_NAME} in
-  "jammy")
-    PKG_VERSION_DOCKER="5:24.0.7-1~ubuntu.22.04~jammy"
-    PKG_VERSION_CONTAINERD="1.6.26-1"
-    ;;
-
-  *)
-    PKG_VERSION_DOCKER=
-    PKG_VERSION_CONTAINERD=
-    ;;
-esac
-
+# setup docker and pin version
 sudo apt update
 sudo DEBIAN_FRONTEND=noninteractive apt install -y -qq --no-install-recommends \
-    docker-ce="${PKG_VERSION_DOCKER}" \
-    docker-ce-cli="${PKG_VERSION_DOCKER}" \
-    containerd.io="${PKG_VERSION_CONTAINERD}"
+    docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo apt-mark hold docker-ce
-sudo apt-mark hold docker-ce-cli
-sudo apt-mark hold containerd.io
+sudo apt-mark hold docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # allow user "ubuntu" to execute "docker" without "sudo"
 sudo usermod -aG docker ubuntu
 
 # ensure docker will start on boot
 sudo systemctl enable docker.service
-
-# set docker bridge ip and restart service
-echo '{"bip": "172.29.0.1/16"}' | sudo tee /etc/docker/daemon.json
-sudo systemctl restart docker.service
-
-# setup docker-compose
-DOCKER_COMPOSE_VERSION="$(curl -sLI https://github.com/docker/compose/releases/latest -o /dev/null -w '%{url_effective}' | cut -d '/' -f 8)"
-sudo curl -o /usr/local/bin/docker-compose \
-          -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)"
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo systemctl start docker.service
 
 # ensure system packages are up to date
 sudo DEBIAN_FRONTEND=noninteractive apt dist-upgrade -y
